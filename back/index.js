4d1444c86c8ca57858e0516c39439c64c6e06ddc
Isaac Njenga <ayzzoh20@gmail.com>
2024-03-28 10:37:10 +0300

    first commit

---
+++ back/index.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const EmployeeModel = require("./models/employee");
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+mongoose.connect(
+  "mongodb+srv://IsaacNjenga:cations!@cluster0.xf14h71.mongodb.net/employee"
+);
+
+app.post("/register", (req, res) => {
+  EmployeeModel.create(req.body)
+    .then((employee) => res.json(employee))
+    .catch((err) => res.json(err));
+});
+
+app.post("/login", (req, res) => {
+  const { email, password } = req.body;
+  EmployeeModel.findOne({ email: email }).then((user) => {
+    if (user) {
+      if (user.password === password) {
+        res.json("Success");
+      } else {
+        res.json("The password is incorrect");
+      }
+    } else {
+      res.json("No record existing");
+    }
+  });
+});
+
+app.listen(3001, () => {
+  console.log("Connected");
+});
