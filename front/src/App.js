8b09d3922651d6039f32f2a2628e1ecc25b7c1d6
Isaac Njenga <111574568+IsaacNjenga@users.noreply.github.com>
2024-03-28 11:48:27 +0300

    Update App.js

--- front/src/App.js
+++ front/src/App.js
@@ -1,7 +1,6 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import SignUp from "./pages/signup";
 import Login from "./pages/login";
-import Home from "./pages/home";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
@@ -9,9 +8,8 @@ function App() {
     <div className="App">
       <BrowserRouter>
         <Routes>
-          <Route path="/register" element={<SignUp />} />
+          <Route path="/" element={<SignUp />} />
           <Route path="/login" element={<Login />} />
-          <Route path="/home" element={<Home />} />
         </Routes>
       </BrowserRouter>
     </div>
